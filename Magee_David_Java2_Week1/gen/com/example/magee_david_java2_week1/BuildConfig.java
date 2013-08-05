ee69275672ff7e4f7ee9384b2643dfd4e18c54e3
David Magee <daveydavinci@fullsail.edu>
2013-08-05 10:51:33 -0400

    Updated with connection to new API set up

---
+++ Magee_David_Java2_Week1/gen/com/example/magee_david_java2_week1/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.magee_david_java2_week1;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
